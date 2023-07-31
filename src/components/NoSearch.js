cbcb623def45202e2d9bdf11dce13f7661f500d6
1KevinScott <134720771+1KevinScott@users.noreply.github.com>
2023-07-31 08:25:06 -0700

    update

--- src/components/NoSearch.js
+++ src/components/NoSearch.js
@@ -1,13 +1,8 @@
 const NoSearch = () => {
-    return (
-      <section className='container'>
-        <h1 className='x-large text-primary'>
-          <i className='fas fa-exclamation-triangle' /> No Search Data Found
-        </h1>
-  
-        <p className='large'>Sorry, there is no search result</p>
-      </section>
-    );
-  };
-  
-  export default NoSearch;
+  return (
+    <section className='container'>
+    </section>
+  );
+};
+
+export default NoSearch;
--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,12 +1,25 @@
+import Box from '@mui/material/Box';
+
 const NotFound = () => {
   return (
-    <section className='container'>
-      <h1 className='x-large text-primary'>
-        <i className='fas fa-exclamation-triangle' /> Page Not Found
-      </h1>
-
-      <p className='large'>Sorry, this page does not exist</p>
-    </section>
+    // <section className='container'>
+    //   <img src="../../public/noSearch.png" alt="dffg"></img>
+    // </section>
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <Box
+        sx={{
+          width: 500,
+          height: 500,
+          backgroundImage: 'url(../../public/noSearch.png)',          // background
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          // '&:hover': {
+          //   backgroundColor: 'primary.main',
+          //   opacity: [0.9, 0.8, 0.7],
+          // },
+        }}
+      />
+    </div>
   );
 };
 
